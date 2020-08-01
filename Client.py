217a2ba33632653090e1cb9461ffdd898bd1d21c
Mahdi Hejrati <m.hejrati20@gmail.com>
2020-08-01 12:19:18 +0430

    99/05/11 12:15 => Update data request in client

--- Client.py
+++ Client.py
@@ -4,20 +4,20 @@ import requests
 
 url = 'http://localhost:8765'
 
+response = requests.get(url)
+print(response.status_code)
+print(response.text)
 
-mydata = {'mykey': 'myvalue'}
+print()
+
+myPostData = {'myPostKey': 'myPostValue'}
 response = requests.post(url, data = mydata)
 print(response.status_code)
 print(response.text)
 
 print()
 
-response = requests.put(url, data = mydata)
-print(response.status_code)
-print(response.text)
-
-print()
-
-response = requests.get(url)
+myPutData = {'myPutKey': 'myPutValue'}
+response = requests.put(url, data = myPutData)
 print(response.status_code)
 print(response.text)
