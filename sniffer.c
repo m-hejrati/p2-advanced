410c44611cc735866cd1663668ac3f200b5ce504
Mahdi Hejrati <m.hejrati20@gmail.com>
2020-08-02 01:36:09 +0430

    99/05/12 01:35 => Change packet_handler method structure in sniffer

--- sniffer.c
+++ sniffer.c
@@ -55,10 +55,10 @@ struct sniff_tcp {
 };
 
 // print readble part of payload
-void print_payload(const u_char *payload, int len) {
+char* find_printable_payload(const u_char *payload, int len) {
 
 	const u_char *ch = payload;
-	char printable[10000];
+	char printable[10000] = "";
 	int j = 0;
 	
 	// find printable character and save them into a new string and then log it
@@ -69,13 +69,41 @@ void print_payload(const u_char *payload, int len) {
 		}
 		ch++;
 	}
-   	syslog(LOG_INFO, "    payload: %s", printable);
+	
+	char* tmp = printable;
+	return tmp;
+}
+
+void print_ip_header(const struct sniff_ip *ip){
+
+	//printf("     Length: %d bytes\n", ntohs(ip->ip_len));
+    syslog(LOG_INFO, "     Length: %d bytes\n", ntohs(ip->ip_len));
+
+	/* print source and destination IP addresses */
+	//printf("       From: %s\n", inet_ntoa(ip->ip_src));
+	syslog(LOG_INFO, "       From: %s\n", inet_ntoa(ip->ip_src));
+
+	//printf("         To: %s\n", inet_ntoa(ip->ip_dst));
+	syslog(LOG_INFO, "         To: %s\n", inet_ntoa(ip->ip_dst));
+
+}
+
+void print_tcp_header(const struct sniff_tcp *tcp, int size_payload){
+
+	//printf("   Src port: %d\n", ntohs(tcp->th_sport));
+	syslog(LOG_INFO, "   Src port: %d\n", ntohs(tcp->th_sport));
+
+	//printf("   Dst port: %d\n", ntohs(tcp->th_dport));
+	syslog(LOG_INFO, "   Dst port: %d\n", ntohs(tcp->th_dport));
+
+	//printf("    Payload: %d bytes\n", size_payload);
+	syslog(LOG_INFO, "    Payload: %d bytes\n", size_payload);
+	
 }
 
 void packet_handler(u_char *args, const struct pcap_pkthdr *packet_header, const u_char *packet_body) {
     
 	/* declare pointers to packet headers */
-	const struct sniff_ethernet *ethernet;  /* The ethernet header [1] */
 	const struct sniff_ip *ip;              /* The IP header */
 	const struct sniff_tcp *tcp;            /* The TCP header */
 	const char *payload;                    /* Packet payload */
@@ -84,72 +112,64 @@ void packet_handler(u_char *args, const struct pcap_pkthdr *packet_header, const
 	int size_tcp;
 	int size_payload;
 
-	/* define ethernet header */
-	ethernet = (struct sniff_ethernet*)(packet_body);
-
 	/* define/compute ip header offset */
 	ip = (struct sniff_ip*)(packet_body + SIZE_ETHERNET);
 	size_ip = IP_HL(ip)*4;
 	if (size_ip < 20) {
-		printf("   * Invalid IP header length: %u bytes\n", size_ip);
+		//printf("   * Invalid IP header length: %u bytes\n", size_ip);
     	syslog(LOG_ERR, " * Invalid IP header length: %u bytes\n", size_ip);
-		//return;
+		return;
 	}
-
-    static int count = 1; // packet counter
-    printf("\nPacket %d logged\n", count);
-    syslog(LOG_INFO, "Packet %d:", count);
-    count++;
-
-	//printf("     Length: %d bytes\n", ntohs(ip->ip_len));
-    syslog(LOG_INFO, "     Length: %d bytes\n", ntohs(ip->ip_len));
-
-	/* print source and destination IP addresses */
-	//printf("       From: %s\n", inet_ntoa(ip->ip_src));
-	syslog(LOG_INFO, "       From: %s\n", inet_ntoa(ip->ip_src));
-	//printf("         To: %s\n", inet_ntoa(ip->ip_dst));
-	syslog(LOG_INFO, "         To: %s\n", inet_ntoa(ip->ip_dst));
-
-	/* determine protocol */	
-	switch(ip->ip_p) {
-		case IPPROTO_TCP:
-			//printf("   Protocol: TCP\n");
-			syslog(LOG_INFO, "   Protocol: TCP\n");
-			break;
-		default:
-			//printf("   Protocol: another\n");
-			syslog(LOG_INFO, "   Protocol: another\n");	
-            break;
-            //return;
-	}
-
 
 	/* define/compute tcp header offset */
 	tcp = (struct sniff_tcp*)(packet_body + SIZE_ETHERNET + size_ip);
 	size_tcp = TH_OFF(tcp)*4;
 	if (size_tcp < 20) {
-		printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+		//printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
     	syslog(LOG_ERR, "   * Invalid TCP header length: %u bytes\n", size_tcp);
 		return;
 	}
-	
-	//printf("   Src port: %d\n", ntohs(tcp->th_sport));
-	syslog(LOG_INFO, "   Src port: %d\n", ntohs(tcp->th_sport));
-	//printf("   Dst port: %d\n", ntohs(tcp->th_dport));
-	syslog(LOG_INFO, "   Dst port: %d\n", ntohs(tcp->th_dport));
-	
+
 	/* define/compute tcp payload (segment) offset */
 	payload = (u_char *)(packet_body + SIZE_ETHERNET + size_ip + size_tcp);
 	
 	/* compute tcp payload (segment) size */
 	size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
 	
-	//printf("    Payload: %d bytes\n", size_payload);
-	syslog(LOG_INFO, "    Payload: %d bytes\n", size_payload);
-
 	//save payload
-	if (size_payload > 0)
-		print_payload(payload, size_payload);
+	if (size_payload > 0){
+		
+		char *printable_payload = find_printable_payload(payload, size_payload);
+
+		if (strstr(printable_payload, "HTTP") != NULL){
+
+			static int count = 1; // packet counter
+			printf("\nPacket %d logged\n", count);
+			syslog(LOG_INFO, "Packet %d:", count);
+			count++;
+
+			/* determine protocol */	
+			switch(ip->ip_p) {
+				case IPPROTO_TCP:
+					syslog(LOG_INFO, "   Protocol: TCP\n");
+					break;
+				case IPPROTO_IP:
+					syslog(LOG_INFO, "   Protocol: IP\n");
+					//return;
+					break;
+				default:
+					printf("   Protocol: unknown\n");
+					return;
+			}
+			print_ip_header(ip);
+
+			if (ip->ip_p == IPPROTO_TCP)
+				print_tcp_header(tcp, size_payload);
+
+			syslog(LOG_INFO, "    payload: %s", printable_payload);
+
+		}
+	}
     return;
 }
 
@@ -195,8 +215,9 @@ int main() {
     char *device; //= "ens33"; // device to sniff on
     pcap_t *handle; // session handle
     char error_buffer[PCAP_ERRBUF_SIZE]; // error string
-    //char filter_exp[] = "tcp port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)"; // filter expression (second part of above expression just filter packet with body)
-    char filter_exp[] = "dst port 8765";
+    char filter_exp[] = "tcp port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)"; // filter expression (second part of above expression just filter packet with body)
+    //char filter_exp[] = "port 8765";
+    char filter_exp[10];
 	struct bpf_program filter; // compiled filter
     bpf_u_int32 subnet_mask, ip;
 	struct pcap_pkthdr header; //header that pcap gives us
